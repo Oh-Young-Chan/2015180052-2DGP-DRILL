cde78390e7ec50159af81a81507872b400ce0188
Oh Young Chan <oyc0507@naver.com>
2019-10-30 13:23:29 +0900

    Drill-09
    2015180052 오영찬
    
    https://youtu.be/ihvaX30PzWA
    commit 횟수 : 1회

--- main_state.py
+++ main_state.py
@@ -5,8 +5,10 @@ import os
 from pico2d import *
 
 import game_framework
-import title_state
-import pause_state
+
+
+from boy import Boy
+from grass import Grass
 
 
 
@@ -18,34 +20,6 @@ font = None
 
 
 
-class Grass:
-    def __init__(self):
-        self.image = load_image('grass.png')
-
-    def draw(self):
-        self.image.draw(400, 30)
-
-
-
-class Boy:
-    def __init__(self):
-        self.x, self.y = 0, 90
-        self.frame = 0
-        self.image = load_image('run_animation.png')
-        self.dir = 1
-
-    def update(self):
-        self.frame = (self.frame + 1) % 8
-        self.x += self.dir
-        if self.x >= 800:
-            self.dir = -1
-        elif self.x <= 0:
-            self.dir = 1
-
-    def draw(self):
-        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
-
-
 def enter():
     global boy, grass
     boy = Boy()
@@ -54,8 +28,9 @@ def enter():
 
 def exit():
     global boy, grass
-    del(boy)
-    del(grass)
+    del boy
+    del grass
+
 
 
 def pause():
@@ -72,20 +47,21 @@ def handle_events():
         if event.type == SDL_QUIT:
             game_framework.quit()
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            game_framework.change_state(title_state)
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_p:
-            game_framework.push_state(pause_state)
+                game_framework.quit()
+        else:
+            boy.handle_event(event)
+
 
 
 def update():
     boy.update()
-
 
 def draw():
     clear_canvas()
     grass.draw()
     boy.draw()
     update_canvas()
+
 
 
 
